89f3b4258b1f3207727b633b266e7b4bf88de79f
Jian Guo <guojian0706@gmail.com>
2020-04-23 16:13:57 -0500

    Prevent invalid input

--- budgety/app.js
+++ budgety/app.js
@@ -80,7 +80,7 @@ var UIController = (function(){
             return {
                 type : document.querySelector(DOMstrings.inputType).value, // Will be either inc or exp
                 description : document.querySelector(DOMstrings.inputDescription).value,
-                value : document.querySelector(DOMstrings.inputValue).value
+                value : parseFloat(document.querySelector(DOMstrings.inputValue).value) // turn string to number
             };
         },
 
@@ -112,6 +112,7 @@ var UIController = (function(){
             fieldsArr.forEach(function(currenValue, index, array){
                 currenValue.value = "";
             })
+            // Place the 
             fieldsArr[0].focus();
             
         },
@@ -139,27 +140,37 @@ var controller = (function(budgetCtrl, UICtrl){
         });
     };
 
+    var updateBudget = function(){
+        // 1. Calculate the budget
 
+        // 2. Return the budget
+
+        // 3. Display the budget on the UI 
+
+    }
     
     var ctrlAddItem = function(){
         var input, newItem
         // 1. Get the field input data
         input = UIController.getInput();
+        // Input description should not be an empty stirng, and input value should be a number > 0
+        if (input.description !== "" && !isNaN(input.value) && input.value > 0){
+            // 2. Add the item to the budget controller
+            newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
-        // 2. Add the item to the budget controller
-        newItem = budgetCtrl.addItem(input.type, input.description, input.value);
-
-        // 3. Add the item to the UI
-        UICtrl.addListItem(newItem,input.type);
-
-
-        // 4. Clear the fields
-        UICtrl.clearFields();
-
-        // 5. Calculate the budget
+            // 3. Add the item to the UI
+            UICtrl.addListItem(newItem,input.type);
 
 
-        // 6. Display the budget on the UI 
+            // 4. Clear the fields
+            UICtrl.clearFields();
+
+            // Calculate and update budget
+            updateBudget();
+        }
+
+        
+
     }
 
     return {
