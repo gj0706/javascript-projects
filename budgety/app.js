63992a59348884f8616985ce1fc4acbbf426d8c6
Jian Guo <guojian0706@gmail.com>
2020-04-23 15:17:44 -0500

    Able to add new items to the UI

--- budgety/app.js
+++ budgety/app.js
@@ -1,4 +1,4 @@
-// BUDGET CONTROLLER
+// BUDGET CONTROLLER: handling data and data structures
 var budgetController = (function(){
 
     // Function constructer
@@ -18,6 +18,7 @@ var budgetController = (function(){
     var data = {
         allItems: {
             exp: [],
+
             inc: []
         },
         totals: {
@@ -67,7 +68,9 @@ var UIController = (function(){
         inputType: '.add__type',
         inputDescription: '.add__description',
         inputValue: '.add__value',
-        inputBtn: '.add__btn'
+        inputBtn: '.add__btn',
+        incomeContainer: '.income__list',
+        expensesContainer: '.expenses__list'
 
     }
     return {
@@ -78,6 +81,27 @@ var UIController = (function(){
                 value : document.querySelector(DOMstrings.inputValue).value
             };
         },
+
+        addListItem: function(obj, type){
+            var html, newHtml, element;
+            // Create HTML string with placeholder text
+            if (type === 'inc'){
+                element = DOMstrings.incomeContainer;
+                html = '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+            }else if (type === 'exp'){
+                element = DOMstrings.expensesContainer;
+                html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+            }
+            // Replace the placeholder text with some actual data
+            newHtml = html.replace('%id%', obj.id);
+            newHtml = newHtml.replace('%description%', obj.description);
+            newHtml = newHtml.replace('%value%', obj.value);
+            // Insert the HTML into the DOM
+            document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
+
+
+        },
+
         getDOMstrings: function(){
             return DOMstrings;
         }
@@ -107,11 +131,12 @@ var controller = (function(budgetCtrl, UICtrl){
         var input, newItem
         // 1. Get the field input data
         input = UIController.getInput();
-        newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
         // 2. Add the item to the budget controller
+        newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
         // 3. Add the item to the UI
+        UICtrl.addListItem(newItem,input.type);
 
         // 4. Calculate the budget
 
@@ -125,6 +150,7 @@ var controller = (function(budgetCtrl, UICtrl){
             setupEventListeners();
         }
     };
+
     
 })(budgetController, UIController);
 
