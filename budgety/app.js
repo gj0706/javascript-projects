325c9541a8ca0a7bf14bf46b295a341759e8f6de
Jian Guo <guojian0706@gmail.com>
2020-04-23 23:04:12 -0500

    Use Event delegation

--- budgety/app.js
+++ budgety/app.js
@@ -111,7 +111,8 @@ var UIController = (function(){
         budgetLabel: '.budget__value',
         incomeLabel: '.budget__income--value',
         expensesLabel: '.budget__expenses--value',
-        percentageLabel: '.budget__expenses--percentage'
+        percentageLabel: '.budget__expenses--percentage',
+        container: '.container'
 
     }
     // Return public methods that can be accessed by all 
@@ -129,10 +130,10 @@ var UIController = (function(){
             // Create HTML string with placeholder text
             if (type === 'inc'){
                 element = DOMstrings.incomeContainer;
-                html = '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+                html = '<div class="item clearfix" id="inc-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
             }else if (type === 'exp'){
                 element = DOMstrings.expensesContainer;
-                html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+                html = '<div class="item clearfix" id="exp-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
             }
             // Replace the placeholder text with some actual data
             newHtml = html.replace('%id%', obj.id);
@@ -191,7 +192,9 @@ var controller = (function(budgetCtrl, UICtrl){
                 ctrlAddItem();
             }
         });
+        document.querySelector(DOM.container).addEventListener('click',ctrlDeleteItem);
     };
+    
 
     var updateBudget = function(){
         // 1. Calculate the budget
@@ -202,7 +205,7 @@ var controller = (function(budgetCtrl, UICtrl){
 
         // 3. Display the budget on the UI 
         UICtrl.displayBudget(budget);
-    }
+    };
     
     var ctrlAddItem = function(){
         var input, newItem
@@ -223,10 +226,25 @@ var controller = (function(budgetCtrl, UICtrl){
             // Calculate and update budget
             updateBudget();
         }
+    };
+    
+    var ctrlDeleteItem = function(event){
+        var itemID, splitID, type, ID;
+        itemID = event.target.parentNode.parentNode.parentNode.parentNode.id;
+        if (itemID){
+            // inc-1
+            splitID = itemID.split('-');
+            type = splitID[0];
+            ID = splitID[1];
 
-        
+            // 1. Delete the item from the data structure
 
-    }
+            // 2. Delete the item from the UI
+
+            // 3. Update and show the new budget
+        }
+
+    };
 
     return {
         // init function: where the application starts
